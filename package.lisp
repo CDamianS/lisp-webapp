7fb42b5b2b701d0124775506186d6782a593a2ad
CDamianS <A01656277@tec.mx>
2023-08-27 18:11:40 -0600

    Started hunchentoot simple functions

--- package.lisp
+++ package.lisp
@@ -2,3 +2,9 @@
 
 (defpackage #:website
   (:use #:cl))
+
+(in-package #:website)
+
+(ql:quickload :hunchentoot)
+(ql:quickload :spinneret)
+(ql:quickload :lass)
--- website.lisp
+++ website.lisp
@@ -1,3 +1,36 @@
 ;;;; website.lisp
-
 (in-package #:website)
+
+(defparameter *server* (make-instance 'hunchentoot:easy-acceptor
+                                      :port 4242))
+
+(defun start ()
+  (tbnl:start *server*))
+
+(defun stop ()
+  (tbnl:stop *server*))
+
+(defparameter *css*
+  '((body
+     :background-color "#1e1e2e"
+     :color "#cdd6f4")))
+
+(tbnl:define-easy-handler (main :uri "/")
+  ()
+  (spinneret:with-html-string
+    (:html
+     (:head
+      (:style
+       (apply #'lass:compile-and-write *css*)))
+     (:body (:h1 "Esto es un sitio web")
+            (:h2 "corriendo en lisp")
+            (:h3 "con los colores de catppuccin")))))
+
+(tbnl:define-easy-handler (stuff :uri "/app")
+    ()
+  (spinneret:with-html-string
+    (:html
+     (:head
+      (:style
+       (apply #'lass:compile-and-write *css*)))
+     (:body (:h1 "The hunchentoot can even handle different indexes")))))
